a671a289e1fc4f0758562491b10c38eb26f50b2e
Jaylin16 <jaylin6115@gmail.com>
2022-10-24 18:49:20 +0900

    서버 연결 완료. 주소 입력 방식 변경 중.

--- delivery/src/main/java/com/project/delivery/DeliveryApplication.java
+++ delivery/src/main/java/com/project/delivery/DeliveryApplication.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 public class DeliveryApplication {
+
+    private InetAddress address;
 
     public static void main(String[] args) {
         SpringApplication.run(DeliveryApplication.class, args);
@@ -18,9 +22,16 @@ public class DeliveryApplication {
     @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
+
+//            private InetAddress serverAddress;
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*");
+
+//                registry.addMapping("/**").allowedOrigins(@Value("${server.address}" String serverAddress){
+//                    this.serverAddress = getAddress();
+//                }
             }
         };
     }
